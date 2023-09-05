9cfd23703f29dc4634ba8b449a9d8a9093982b72
Valentin Lucero <hamiltonprop03@gmail.com>
2023-09-05 08:25:38 -0500

    Creamos los alert y generamos el subtotal de la compra

--- main.js
+++ main.js
@@ -18,51 +18,63 @@ function saludo() {
 
 
 
-
-
-
 //Compra final.
 function comprarGorras() {
     let gorras = '';
     let talles = '';
+    let cantidad = 0;
     let precio = 0;
+    let subtotal = 0;
     let seguirComprando = true; 
 
     do {
-        gorras = prompt('Que tipo de gorra le gustaria comprar: Caps, Sunhats, Beanies o Pilusos?');
-        talles = prompt('Que talle prefiere: S, M, L o XL?');
+        gorras = prompt('Que tipo de gorra le gustaria comprar: Caps, Sunhats, Beanies o Pilusos?').toLowerCase();
+        talles = prompt('Que talle prefiere: S, M, L o XL?').toUpperCase();
+        cantidad = parseInt(prompt('Cuantas gorras deseas comprar?'));
 
         console.log(gorras)
         console.log(talles)
 
         switch (gorras) { //Utilizamos una sentencia switch para verificar diferentes opciones del usuario
-            case 'Caps':
+            case 'caps':
                 precio = 19.29;
                 alert('El precio de las Caps es de: $19.29');
                 break;
-            case 'Sunhats':
+            case 'sunhats':
                 precio = 29.99;
                 alert('El precio de los Sunhats es de: $29.99');
                 break;
-            case 'Beanies':
+            case 'beanies':
                 precio = 29.99;
                 alert('El precio de los Beanies es de: $29.99');
                 break;
-            case 'Pilusos':
+            case 'pilusos':
                 precio = 19.99;
                 alert('El precio de los pilusos es de: $19.99');
                 break;
             default:
                 precio = 0;
+                cantidad = 0;
                 alert('Haz ingresado algun dato incorrectamente, intentalo de nuevo!');
-                break;
+                continue;
         }
+
+        subtotal += precio * cantidad;
+        console.log(subtotal);
 
         seguirComprando = confirm('Desea seguir comprando? Seleccione aceptar si su respuesta es afirmativa o cancelar si es negativa.'); //Utilizamos el metodo confirm para detener la compra del usuario y poder salirnos del bucle.
 
     } while (seguirComprando);
-}
 
+    //Alert sobre la compra total
+    alert('Su compra final es: ' + cantidad + ' ' + gorras + ' de talle ' + talles);
+    //Alert sobre el precio final
+    alert('Su precio final es de: ' + subtotal);
+    //Saludo de despedida
+    alert('Gracias por confiar en CapStyle, lo esperamos pronto!!');
+    
+    return subtotal;
+}
 
 saludo();
 
@@ -70,3 +82,5 @@ saludo();
 let alertPresentacion = alert('En CapStyle nos diferenciamos por nuestro detalle y empeño que le ponemos a la customizacion de gorras.');
 
 comprarGorras();
+
+
